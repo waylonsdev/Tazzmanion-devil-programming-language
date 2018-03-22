42e83757bf81ad2ab7ce00e23ee034a284863293
waylon knudsen <waylonsdev@gmail.com>
2018-03-21 21:05:20 -0600

    v1.0.0.1
    working on the kex.

--- basic.py
+++ basic.py
@@ -5,8 +5,27 @@ def open_file(filename):
     return data
 
 def lex(filecontents):
+    tok = ""
+    state = 0
+    string = ""
     filecontents = list(filecontents)
-    print(filecontents)
+    for char in filecontents:
+        tok += char
+        if tok == " ":
+            tok = ""
+        elif tok == "PRINT":
+            print("FOUND A PRINT")
+            tok = ""
+        elif tok == "\"":
+            if state == 0:
+                state = 1
+            elif state == 1:
+                print("FOUND A STRING")
+                string = ""
+                state = 0
+        elif state == 1:
+            string += char
+            tok = ""
 
 def run():
     data = open_file(argv[1])
